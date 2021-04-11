a72211ea11331f7b489fc13fb3c2bf9e8cbfaf95
Vrinda Menon <vrindanm@gmail.com>
2021-04-11 20:43:39 +0530

    menu icon issue resolve - logo float left

--- resources/scripts/events.js
+++ resources/scripts/events.js
@@ -1,13 +1,16 @@
 let primaryNavMobileView = document.getElementById("primary-nav-mobile-view");
+let menuIcon = document.getElementById("menu-icon");
 
 function closeMenu(){
 
     primaryNavMobileView.style.width = "0" ;
+    menuIcon.style.display = "block";
 
 }
 
 function openMenu(){
 
-    primaryNavMobileView.style.width = "40%";
+    primaryNavMobileView.style.width = "80%";
+    menuIcon.style.display = "none";
 
 }
