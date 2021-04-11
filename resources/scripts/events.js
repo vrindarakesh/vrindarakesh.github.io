4a522ad3e2fe6b24a71fa757decee195c010431a
Vrinda Menon <vrindanm@gmail.com>
2021-04-11 17:33:21 +0530

    Rectify design flaws

---
+++ resources/scripts/events.js
@@ -0,0 +1,13 @@
+let primaryNavMobileView = document.getElementById("primary-nav-mobile-view");
+
+function closeMenu(){
+
+    primaryNavMobileView.style.width = "0" ;
+
+}
+
+function openMenu(){
+
+    primaryNavMobileView.style.width = "40%";
+
+}
